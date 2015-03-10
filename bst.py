a0052b0628f92b2ab561dc41966f629d794ed4e9
Mark Saiget <saiget@gmail.com>
2015-03-09 18:49:09 -0700

    Add parent value to each node.

--- bst.py
+++ bst.py
@@ -12,13 +12,14 @@ class bst():
         if node < val:
             if self.nodes[node].get('right') is None:
                 self.nodes[node]['right'] = val
-                self.nodes[val] = {}
+                self.nodes[val] = {'parent': node}
+
             else:
                 self._insert(self.nodes[node]['right'], val)
         else:
             if self.nodes[node].get('left') is None:
                 self.nodes[node]['left'] = val
-                self.nodes[val] = {}
+                self.nodes[val] = {'parent': node}
             else:
                 self._insert(self.nodes[node]['left'], val)
 
