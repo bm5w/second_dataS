e66ccf69728ea7276259084fc6616d685a7aa847
Mark Saiget <saiget@gmail.com>
2015-03-12 13:16:44 -0700

    Add hash.py file.

---
+++ hash.py
@@ -0,0 +1,18 @@
+'''An implementation of a hash table.'''
+
+class HashTable(object):
+    def __init__(self, size=1024):
+        '''Initialize a Hashtable of a given size.'''
+        pass
+
+    def get(self, key):
+        '''Return the value stored with the given key.'''
+        pass
+
+    def set(self, key, val):
+        '''Store the given val using the given key.'''
+        pass
+
+    def hash(self, key):
+        '''Hash the key provided.'''
+        pass
