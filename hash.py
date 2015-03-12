434dbbcf39426645bfc149d25bd7b943e132d979
Mark Saiget <saiget@gmail.com>
2015-03-12 14:44:51 -0700

    Add tests.

--- hash.py
+++ hash.py
@@ -3,28 +3,28 @@ class HashTable(object):
 
     def __init__(self, size=1024):
         '''Initialize a Hashtable of a given size.'''
-        self.table = [[]] * size
+        self.table = [[] for x in range(size)]
         self.size = size
 
     def hash(self, key):
         '''Hash the key provided.'''
-        self.val = 0
+        val = 0
         if type(key) is not str:
             raise TypeError('Key must be a string.')
-        for character in key: 
-            val =+ ord(character)
+        for character in key:
+            val += ord(character)
         return val % self.size
 
     def set(self, key, val):
         '''Store the given val using the given key.'''
-        try: 
-            self.table[self.hash(key)].append((key, value))
+        try:
+            self.table[self.hash(key)].append((key, val))
         except (NameError, TypeError):
             raise TypeError("Must use a string")
 
     def get(self, key):
         '''Return the value stored with the given key.'''
-        for x, y in self.table[self.hash(key)].iteritems():
-            if key == x:
-                return y
-        raise Error('Key not found.')
+        for entries in self.table[self.hash(key)]:
+            if key == entries[0]:
+                return entries[1]
+        raise NameError('Key not found.')
