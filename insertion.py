99849b4f2568b42fa01578a24f5a5b17ad350916
Mark Saiget <saiget@gmail.com>
2015-03-16 18:17:01 -0700

    Add a few tests.

--- insertion.py
+++ insertion.py
@@ -1,19 +1,27 @@
-def insertion(list):
-    '''Sorts a list via the insertion method.'''
-    for i in range(1, len(list)):
+import timeit
 
-        key = list[i]
+def insertion(_list):
+    '''Sorts a list via the insertion method.'''
+    if type(_list) is not list:
+        raise TypeError('Entire list must be numbers')
+    for i in range(1, len(_list)):
+
+        key = _list[i]
+        if not isinstance(key, int):
+            raise TypeError('Entire list must be numbers')
         position = i
 
-        while position > 0 and list[position-1] > key:
-            list[position] = list[position-1]
+        while position > 0 and _list[position-1] > key:
+            _list[position] = _list[position-1]
             position = position-1
 
-        list[position] = key
+        _list[position] = key
 
-    return list
+    return _list
 
 if __name__ == '__main__':
     input = [1, 3, 2]
     output = insertion(input)
     print output
+
+    timeit.timeit(insertion(input))
--- test_insertion.py
+++ test_insertion.py
@@ -1,6 +1,29 @@
 import pytest
 from insertion import insertion
 
+
 def test_insertion():
     input = [54,26,93,17,71,31,44,55,20]
     assert insertion(input) == [17,20,26,31,44,54,55,71,93]
+
+
+def test_insertion_big():
+    input = range(10000)
+    assert insertion(input) == range(10000)
+
+
+def test_wrong_type():
+    input = 'x'
+    with pytest.raises(TypeError):
+        insertion(input)
+
+
+def test_wrong_type_in_list():
+    input = [1, 2, 'a']
+    with pytest.raises(TypeError):
+        insertion(input)
+
+
+def test_insertion_big_reverse():
+    input = range(10000)[::-1]
+    assert insertion(input) == range(10000)
