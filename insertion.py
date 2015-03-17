cf2615c2488198bd9f904a4e65ac4fc0e0d6c475
Mark Saiget <saiget@gmail.com>
2015-03-16 18:35:54 -0700

    Add timing to show time complexity.

--- insertion.py
+++ insertion.py
@@ -1,5 +1,18 @@
-import timeit
+import time
 
+
+def timed_func(func):
+    """Decorator for timing our traversal methods."""
+    def timed(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        elapsed = time.time() - start
+        # print "time expired: %s" % elapsed
+        return (result, elapsed)
+    return timed
+
+
+@timed_func
 def insertion(_list):
     '''Sorts a list via the insertion method.'''
     if type(_list) is not list:
@@ -19,9 +32,35 @@ def insertion(_list):
 
     return _list
 
-if __name__ == '__main__':
-    input = [1, 3, 2]
-    output = insertion(input)
-    print output
 
-    timeit.timeit(insertion(input))
+
+if __name__ == '__main__':
+    lengths = [10, 100, 1000, 10000]
+    times = []
+    for x in lengths:
+        output = insertion(range(x))
+        times.append(output[1])
+    print 'Best case scenario:'
+    for length, tim in zip(lengths, times):
+        print 'a list of length {} was sorted in {}'.format(length, tim)
+    diff = []
+    for x in range(len(times)-2):
+        diff.append(times[x+1]/times[x])
+    average = reduce(lambda x, y: x+y, diff) / len(diff)
+    print 'As length increases by 10, time increases by {}'.format(average)
+
+    lengths = [10, 100, 1000, 10000]
+    times = []
+    for x in lengths:
+        output = insertion(range(x)[::-1])
+        times.append(output[1])
+    print 'Worse case scenario:'
+    for length, tim in zip(lengths, times):
+        print 'a list of length {} was sorted in {}'.format(length, tim)
+    diff = []
+    for x in range(len(times)-2):
+        diff.append(times[x+1]/times[x])
+    average = reduce(lambda x, y: x+y, diff) / len(diff)
+    print 'As length increases by 10, time increases by {}'.format(average)
+
+
