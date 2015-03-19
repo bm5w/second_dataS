4934b2992b5603cbfcc0d7b21c162e4bee79e640
jacquestardie <hi@jacquestardie.org>
2015-03-19 15:48:50 -0700

    move buckets back into a single list

--- radix.py
+++ radix.py
@@ -1,21 +1,33 @@
 def radixsort(input):
     base = 10  # The number of buckets to use.
+    place = 1  # IE, the 1's, 10's, or 100's place.
 
-    # Declare the number of buckets to use and create them.
-    buckets = [list() for _ in range(base)]
+    complete = False
+    while not complete:
+        complete = True
 
-    # Segment the input list into appropriate buckets.
-    for item in input:
+        # Declare the number of buckets to use and create them.
+        buckets = [list() for _ in range(base)]
 
-        # For each item in the list, append it into a bucket.
-        # To determine which bucket it should go into, divide the item
-        # by the base of the dataset in use.
-        # IE: 
-        # - an item of value 1 % 10 --> bucket 1
-        # - an item of value 5 % 10 --> bucket 5
-        buckets[item % base].append(item)
+        # Segment the input list into appropriate buckets.
+        for item in input:
+            hold = item / place
 
-    # Reassign the buckets into a single list.
+            # For each item in the list, append it into a bucket.
+            # To determine which bucket it should go into, divide the item
+            # by the base of the dataset in use.
+            # IE: 
+            # - an item of value 1 % 10 --> bucket 1
+            # - an item of value 5 % 10 --> bucket 5
+            buckets[hold % base].append(item)
+
+        # Reassign the buckets into a single list.
+        value = 0
+        for bucketIndex in range(base):
+            placeholder = buckets[bucketIndex]
+            for item in placeholder:
+                input[value] = item
+                value += 1
 
 if __name__ == "__main__":
     import time
