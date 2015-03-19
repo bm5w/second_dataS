36b80f86ce7050d05e7c4256922375f618538e85
Mark Saiget <saiget@gmail.com>
2015-03-19 01:00:07 -0700

    Working in-place list quicksort.

--- quicksort.py
+++ quicksort.py
@@ -4,12 +4,10 @@ class Quicksort(object):
     def __init__(self, input_list):
         if type(input_list) is not list:
             raise TypeError('Input type must be list.')
-
         self.input_list = input_list
 
     def swap(self, first, second):
         self.input_list[first], self.input_list[second] = self.input_list[second], self.input_list[first]
-        print 'swap {}'.format(self.input_list)
 
     def quicksort(self):
         self._quicksort(0, len(self.input_list)-1)
@@ -17,10 +15,11 @@ class Quicksort(object):
 
     def _find_pivot(self, start, finish):
         """Return index of median of start, finish, and middle values."""
+        if (finish-start) <= 3:
+            return finish
         a = self.input_list[start]
         b = self.input_list[((finish-start)/2)+start]
         c = self.input_list[finish]
-
         if a < b:
             if b < c:
                 return ((finish-start)/2)+start
@@ -28,28 +27,17 @@ class Quicksort(object):
             if a < c:
                 return start
         return finish
-        return start
 
     def _quicksort(self, start, finish):
-        print self.input_list
-        if 2 > finish-start:
-            return
-        pivot_index = self._find_pivot(start, finish)
-        self.swap(pivot_index, finish)
-        pivot = self.input_list[finish]
-        center = start
-        index = start
-        count = 0
-        gt_or_eq = 0
-
-        while count <= (finish-start):
-            if self.input_list[index] < pivot:     # LESS THAN
-                center += 1
-                index += 1
-            else:                                     # GREATER THAN or EQUAL
-                self.swap(index, finish-gt_or_eq)
-                gt_or_eq += 1
-            count += 1
-
-        self._quicksort(start, center)
-        self._quicksort(center, finish)
+        """Recursive helper method for quicksort."""
+        if finish > start:
+            pivot_index = self._find_pivot(start, finish)
+            pivot = self.input_list[pivot_index]
+            center = start
+            for index in xrange(start, finish):
+                if self.input_list[index] <= pivot:
+                    self.swap(index, center)
+                    center += 1
+            self.swap(center, finish)
+            self._quicksort(start, center-1)
+            self._quicksort(center, finish)
--- test_quicksort.py
+++ test_quicksort.py
@@ -13,6 +13,18 @@ def test_quicksort():
     assert Quicksort(input).quicksort() == [17, 20, 26, 31, 44, 54, 55, 71, 93]
 
 
+def test_quicksort_duplicate():
+    input = [54, 26, 93, 17, 71, 31, 44, 55, 20, 20]
+
+    assert Quicksort(input).quicksort() == [17, 20, 20, 26, 31, 44, 54, 55, 71, 93]
+
+
+def test_quicksort_duplicate():
+    input = [54, 26, 93, 17, 71, 31, 44, 55, 20, 20, 20]
+
+    assert Quicksort(input).quicksort() == [17, 20, 20, 20, 26, 31, 44, 54, 55, 71, 93]
+
+
 def test_quicksort_large():
     input = range(1000)
 
@@ -37,23 +49,23 @@ def test_quicksort_big_reverse():
     assert Quicksort(input).quicksort() == range(1000)
 
 
-# def test_quicksort_big_increase_decrease():
-#     input = range(500)+range(500)[::-1]
-#     expected = range(500)*2
-#     expected.sort()
+def test_quicksort_big_increase_decrease():
+    input = range(500)+range(500)[::-1]
+    expected = range(500)*2
+    expected.sort()
 
-#     assert Quicksort(input).quicksort() == expected
-
-
-# def test_quicksort_duplicates():
-#     input = (range(100)+range(100))[::-1]
-#     expected = range(100)+range(100)
-#     expected.sort()
-
-#     assert Quicksort(input).quicksort() == expected
+    assert Quicksort(input).quicksort() == expected
 
 
-# def test_quicksort_all_duplicates():
-#     input = [100]*20
+def test_quicksort_duplicates():
+    input = (range(100)+range(100))[::-1]
+    expected = range(100)+range(100)
+    expected.sort()
 
-#     assert Quicksort(input).quicksort() == [100]*20
+    assert Quicksort(input).quicksort() == expected
+
+
+def test_quicksort_all_duplicates():
+    input = [100]*20
+
+    assert Quicksort(input).quicksort() == [100]*20
